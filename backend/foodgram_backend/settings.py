3a964936f36a3a9cbea661426a0419c41a79bf07
Станислав Ефимов <stanislav.karov@mail.ru>
2024-05-24 23:24:04 +0300

    Change default permission class. Delete unnecessary import

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -1,7 +1,6 @@
 from dotenv import load_dotenv
 
 import os
-from datetime import timedelta
 from pathlib import Path
 
 
@@ -118,7 +117,7 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.AllowAny',
+        'rest_framework.permissions.IsAuthenticated',
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
